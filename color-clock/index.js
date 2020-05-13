e2676dac3f87cf2719c4f3c406032d46b627271a
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-13 05:05:44 +0000

    Automatically backed up by Learn

--- color-clock/index.js
+++ color-clock/index.js
@@ -20,7 +20,7 @@ function displayTime(){
     clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
     clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
     color.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16)
-  }, 1000)
+  }, 2000)
 }
 
 window.onload = displayTime()
